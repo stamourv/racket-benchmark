ee6e9a4cf867023efc8c6151b76b2316de907faf
Josh McGrath <josh.mcgrath08@gmail.com>
2013-10-17 15:20:16 -0400

    Don't overwrite previous benchmark results
    - Instead use the file name provided by user as a base name,
      appending an integer to the end for uniqueness.
    - get-past-results by default will return the newest version
      of the benchmark results, unless the version argument is specified,
      in which case it returns the results associated with that version
    - Fixes #9

--- benchmark/results.rkt
+++ benchmark/results.rkt
@@ -3,12 +3,49 @@
 (provide get-past-results
          record-results)
 
-;; path? -> (listof benchmark-result?)
-(define (get-past-results file)
-  (if (file-exists? file)
-      (file->value file #:mode 'text)
-      (error "get-past-results: file not found" file)))
+;; path? exact-integer? -> (listof benchmark-result?)
+(define (get-past-results file [version #f])
+  (file->value (get-file file version) #:mode 'text))
 
 ;; (listof benchmark-result?) path? -> void?
 (define (record-results results file)
-  (write-to-file results file #:mode 'text #:exists 'truncate))
+  (let ([fresh-name (mk-fresh-file-name file)])
+    (write-to-file results fresh-name #:mode 'text #:exists 'truncate)
+    (displayln (format "Wrote results to ~a" fresh-name))))
+
+;; string? (or/c exact-integer? #f) -> string?
+;; get latest version of file-base if version #f
+;; if version not #f, get (fmt file-base version)
+(define (get-file file-base version)
+  (define (get-file-names [v 0])
+    (let ([file-name (fmt file-base v)])
+      (if (file-exists? file-name)
+          (cons file-name (get-file-names (+ 1 v)))
+          (list))))
+  (let ([file-names (get-file-names)])
+    (cond
+     ;; no specific version and file-names not null
+     [(and (not version) (not (null? file-names)))
+      (last file-names)]
+     ;; specific version and assoc. file exists
+     [(and version (file-exists? (fmt file-base version)))
+      (fmt file-base version)]
+     ;; specific version and assoc. file doesn't exist
+     [version
+      (error 'get-file "No file found: ~a" (fmt file-base version))]
+     ;; no specific version, but no matching files exist
+     [else
+      (error 'get-file "No files found matching ~a-([0-9]+)"
+              (fmt file-base version))])))
+
+;; string? [exact-integer?] -> string?
+;; create a new fresh file name of the form file-base-<n>
+(define (mk-fresh-file-name file-base [version 0])
+  (if (file-exists? (fmt file-base version))
+      (mk-fresh-file-name file-base (+ version 1))
+      (fmt file-base version)))
+
+;; string? exact-integer? -> string?
+;; format a file name given the file-base and version
+(define (fmt file-base version)
+  (format "~a-~a" file-base version))
