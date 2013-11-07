2022a5b8820c7277fe2f92b6eb6ba9a9dc667683
Josh McGrath <josh.mcgrath08@gmail.com>
2013-11-07 17:44:25 -0500

    Missed one mk-bench-opts

--- benchmark/examples/sanity/sanity-run.rkt
+++ benchmark/examples/sanity/sanity-run.rkt
@@ -3,8 +3,8 @@
 (require benchmark "linux-time.rkt" "sanity-common.rkt")
 
 (define (sleep-internal-bench n)
-  (bench-one (format "sleep ~a" n)
-             (sleep n) (make-bench-opts #:itrs-per-trial 1)))
+  (parameterize ([itrs-per-trial 1])
+    (bench-one (format "sleep ~a" n) (sleep n))))
 
 (define times (list .0001 .001 .01 .1 .5 1 1.3 1.67 2))
 
