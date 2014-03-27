ddd86af24cf00005ac476f3078acc788d7da6994
Vincent St-Amour <stamourv@racket-lang.org>
2014-03-27 18:22:54 -0400

    Try to fix gh-pages script.

--- github-docs.sh
+++ github-docs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # run from this directory
-scribble --html --dest . --dest-name index.html +m --redirect-main http://docs.racket-lang.org benchmark/scribblings/benchmark.scrbl
+mkdir -p scratch-doc
+scribble --html --dest scratch-doc --dest-name index.html +m --redirect-main http://docs.racket-lang.org benchmark/scribblings/benchmark.scrbl
 git checkout gh-pages
+mv scratch-doc/* .
 git add *.html *.css *.js *.png
 git commit -am 'Update github docs.'
 git push
