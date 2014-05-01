beb534a2a80263af64f73775d719cb9af45c04f1
Vincent St-Amour <stamourv@racket-lang.org>
2014-05-01 14:43:17 -0400

    Add version number.
    Not sure API is quite right yet, so sub 1.0.

--- info.rkt
+++ info.rkt
@@ -4,3 +4,9 @@
 (define deps '("base" "math-lib" "plot-gui-lib" "plot-lib" "srfi-lite-lib"
                "typed-racket-lib"))
 (define build-deps '("plot-doc" "racket-doc" "rackunit-lib" "scribble-lib"))
+
+(define pkg-desc "Benchmarking library.")
+
+(define version "0.4")
+
+(define pkg-authors '(mcgrathj stamourv))
