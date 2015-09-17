d568fe56d1e4b5296fa54160055482f9c125bb06
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-17 15:25:23 -0500

    Remove unused dependency.

--- info.rkt
+++ info.rkt
@@ -1,8 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "math-lib" "plot-gui-lib" "plot-lib" "srfi-lite-lib"
-               "typed-racket-lib"))
+(define deps '("base" "math-lib" "plot-gui-lib" "plot-lib" "typed-racket-lib"))
 (define build-deps '("plot-doc" "racket-doc" "rackunit-lib" "scribble-lib"))
 
 (define pkg-desc "Benchmarking library.")
